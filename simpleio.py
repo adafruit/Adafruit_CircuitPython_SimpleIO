442dd8ea7921f804c83cce413bcd70b029df21a2
brentru <robots199@me.com>
2017-07-26 09:58:44 -0400

    added shiftinout

--- simpleio.py
+++ simpleio.py
@@ -31,8 +31,72 @@ The `simpleio` module contains classes to provide simple access to IO.
 """
 
 import digitalio
-import math
+import time
+
 from neopixel_write import neopixel_write
+
+def shiftIn(dataPin, clock, msb_first=True):
+    """
+    Shifts in a byte of data one bit at a time. Starts from either the LSB or
+    MSB.
+
+    :param dataPin: pin on which to input each bit
+    :param clock: toggles to signal dataPin reads
+    :param msb_first: order to shift bits (least significant or most significant bit first)
+    :return: returns the value read
+    :rtype: int
+    """
+
+    value = 0
+    i = 0
+
+    for i in range(0, 8):
+        clock.value = True
+        if msb_first == False:
+            value |= ((dataPin.value) << i)
+        else:
+            value |= ((dataPin.value) << (7-i))
+        clock.value = False
+        i+=1
+    return value
+
+def shiftOut(dataPin, clock, value, msb_first=True):
+    """
+    Shifts out a byte of data one bit at a time. Data gets written to a data
+    pin. Then, the clock pulses hi then low
+
+    :param dataPin: value bits get output on this pin
+    :param clock: toggled once the data pin is set
+    :param msb_first: order to shift bits (least significant or most significant bit first)
+    :param value: byte to be shifted
+
+    Example for Metro Express:
+
+    .. code-block:: python
+
+        import digitalio
+        import simpleio
+        from board import *
+        clock = digitalio.DigitalInOut(D12)
+        dataPin = digitalio.DigitalInOut(D11)
+        clock.direction = digitalio.Direction.OUTPUT
+        dataPin.direction = digitalio.Direction.OUTPUT
+
+        while True:
+            valueSend = 500
+            shiftOut(dataPin, clock, 'LSBFIRST', (valueSend>>8))
+            shiftOut(dataPin, clock, 'LSBFIRST', valueSend)
+            shiftOut(dataPin, clock, 'MSBFIRST', (valueSend>>8))
+            shiftOut(dataPin, clock, 'MSBFIRST', valueSend)
+    """
+    value = value&0xFF
+    for i in range(0, 8):
+        if msb_first == False:
+            tmpval = bool((value & (1 << i)))
+            dataPin.value = tmpval
+        else:
+            tmpval = bool(value & (1 << (7-i)))
+            dataPin.value = tmpval
 
 class DigitalOut:
     """
