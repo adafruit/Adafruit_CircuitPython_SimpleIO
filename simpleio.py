a53c2bcf0e4f9d1cfcdda157e850ae286dd8b77d
Asher Lieber <lieber.asher@gmail.com>
2017-07-28 15:23:22 -0400

    map_range function comparable to arduino's map() (#14)
    Add map_range to mimic Arduino's map().

--- simpleio.py
+++ simpleio.py
@@ -159,3 +159,12 @@ class NeoPixel:
 
     def __len__(self):
         return len(self.buf) // self.bpp
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    """
+    Maps a number from one range to another.
+    Note: This implementation handles values < in_min differently than arduino's map function does.
+    :return: Returns value mapped to new range
+    :rtype: float
+    """
+    return max(min((x-in_min) * (out_max - out_min) / (in_max-in_min) + out_min, out_max), out_min)
