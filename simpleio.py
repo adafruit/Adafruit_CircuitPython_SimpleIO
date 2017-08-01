32924da682b6678a7ace2bdef0f7aadfaa1d4baa
brentru <robots199@me.com>
2017-08-01 12:48:18 -0400

    Added easy control for hobby servos

--- simpleio.py
+++ simpleio.py
@@ -27,6 +27,10 @@ The `simpleio` module contains classes to provide simple access to IO.
 """
 
 import digitalio
+<<<<<<< HEAD
+=======
+import pulseio
+>>>>>>> Added easy control for hobby servos
 import math
 import time
 
@@ -101,6 +105,70 @@ def shift_out(dataPin, clock, value, msb_first=True):
             tmpval = bool((value & (1 << i)))
             dataPin.value = tmpval
 
+<<<<<<< HEAD
+=======
+class Servo:
+    """
+    Easy control for hobby (3-wire) servos
+
+    :param ~pulseio.PWMOut servoPin: PWM pin where the servo is located.
+    :param int servoMin: Minimum amount of microseconds allowed. Varies depending on type of servo.
+    :param int servoMax: Maximum amount of microseconds allowed. Varies depending on type of servo.
+
+    Example for Metro M0 Express:
+
+    .. code-block:: python
+
+        import simpleio
+        import time
+        from board import *
+
+        # attach a servo to pin Digital 10
+        myServo = simpleio.Servo(D10)
+
+        while True:
+            # write an angle of 90deg to the servo object
+            myServo.writeAngle(90)
+            time.sleep(1)
+            # write an angle of 150deg to the servo object
+            myServo.writeAngle(150)
+            # print the location of the servo
+            print(myServo.read())
+            time.sleep(1)
+            # write to the servo in microseconds
+            myServo.writeMicroseconds(5500)
+            time.sleep(1)
+    """
+    def __init__(self, servoPin, servoMin = 0.5, servoMax = 2.5):
+        self.myServo = pulseio.PWMOut(servoPin, frequency = 50)
+        self.servoMin = servoMin
+        self.servoMax = servoMax
+        # prevent weird values due to read before write calls
+        self.angle = 90
+
+    def writeAngle(self, angle):
+        """Writes a value in degrees to the servo"""
+        self.angle = max(min(180, angle), 0)
+        pulseWidth = 0.5 + (self.angle / 180) * (self.servoMax - self.servoMin)
+        dutyPercent = pulseWidth / 20.0
+        self.myServo.duty_cycle = int(dutyPercent * 65535)
+
+    def writeMicroseconds(self, uS):
+        """Writes a value in microseconds to the servo"""
+        ms = (uS/1000)
+        ms = max(min(self.servoMax, ms), self.servoMin)
+        dutyPercent = ms / 20.0
+        self.myServo.duty_cycle = int(dutyPercent * 65535)
+
+
+    def detach(self):
+        """Detaches servo object from pin, frees pin"""
+        self.myServo.detach()
+
+    def read(self):
+        return self.angle
+
+>>>>>>> Added easy control for hobby servos
 class DigitalOut:
     """
     Simple digital output that is valid until soft reset.
