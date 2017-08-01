50dd6c354bc9082ea96fcaa82f9a1974e6bc2cd5
Scott Shawcroft <scott@chickadee.tech>
2017-07-31 18:23:44 -0700

    Correct example in docs and add warning about pin order.

--- simpleio.py
+++ simpleio.py
@@ -35,6 +35,9 @@ def shift_in(dataPin, clock, msb_first=True):
     Shifts in a byte of data one bit at a time. Starts from either the LSB or
     MSB.
 
+    .. warning:: Data and clock are swapped compared to other CircuitPython libraries
+      in order to match Arduino.
+
     :param ~digitalio.DigitalInOut dataPin: pin on which to input each bit
     :param ~digitalio.DigitalInOut clock: toggles to signal dataPin reads
     :param bool msb_first: True when the first bit is most significant
@@ -60,6 +63,9 @@ def shift_out(dataPin, clock, value, msb_first=True):
     Shifts out a byte of data one bit at a time. Data gets written to a data
     pin. Then, the clock pulses hi then low
 
+    .. warning:: Data and clock are swapped compared to other CircuitPython libraries
+      in order to match Arduino.
+
     :param ~digitalio.DigitalInOut dataPin: value bits get output on this pin
     :param ~digitalio.DigitalInOut clock: toggled once the data pin is set
     :param bool msb_first: True when the first bit is most significant
@@ -80,11 +86,11 @@ def shift_out(dataPin, clock, value, msb_first=True):
         while True:
             valueSend = 500
             # shifting out least significant bits
-            shift_out(dataPin, clock, (valueSend>>8), msb_first = False)
-            shift_out(dataPin, clock, valueSend, msb_first = False)
+            simpleio.shift_out(dataPin, clock, (valueSend>>8), msb_first = False)
+            simpleio.shift_out(dataPin, clock, valueSend, msb_first = False)
             # shifting out most significant bits
-            shift_out(dataPin, clock, (valueSend>>8))
-            shift_out(dataPin, clock, valueSend)
+            simpleio.shift_out(dataPin, clock, (valueSend>>8))
+            simpleio.shift_out(dataPin, clock, valueSend)
     """
     value = value&0xFF
     for i in range(0, 8):
