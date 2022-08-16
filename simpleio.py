eeb165ca4852fb74d755e9dae451d820ac4863f6
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:16 -0400

    Update version string

--- simpleio.py
+++ simpleio.py
@@ -30,7 +30,7 @@ try:
 except ImportError:
     pass  # not always supported by every board!
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_SimpleIO.git"
 
 
